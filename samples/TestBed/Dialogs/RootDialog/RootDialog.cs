6d4792d6254682df0c75e490fcb9a4577f86883a
Vishwac Sena Kannan <vkannan@microsoft.com>
2019-07-17 11:50:33 -0700

    Getting 4.6 preview branch with one sample

---
+++ samples/TestBed/Dialogs/RootDialog/RootDialog.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.Dialogs.Adaptive;
+using Microsoft.Bot.Builder.Dialogs.Adaptive.Rules;
+using Microsoft.Bot.Builder.Dialogs.Adaptive.Steps;
+using Microsoft.Bot.Builder.LanguageGeneration;
+
+namespace Microsoft.BotBuilderSamples
+{
+    public class RootDialog : ComponentDialog
+    {
+        public RootDialog()
+            : base(nameof(RootDialog))
+        {
+            // Create instance of adaptive dialog. 
+            var rootDialog = new AdaptiveDialog(nameof(AdaptiveDialog))
+            {
+                // These steps are executed when this Adaptive Dialog begins
+                Rules = new List<IRule>()
+                {
+                    // Add a rule to welcome user
+                    new ConversationUpdateActivityRule()
+                    {
+                        Steps = WelcomeUserSteps()
+                    }
+                },
+                Generator = new TemplateEngineLanguageGenerator(new TemplateEngine().AddFile(Path.Combine(".", "Dialogs", "RootDialog", "RootDialog.lg")))
+            };
+
+            // Add named dialogs to the DialogSet. These names are saved in the dialog state.
+            AddDialog(rootDialog);
+
+            // The initial child Dialog to run.
+            InitialDialogId = nameof(AdaptiveDialog);
+        }
+        private static List<IDialog> WelcomeUserSteps()
+        {
+            return new List<IDialog>()
+            {
+                // Iterate through membersAdded list and greet user added to the conversation.
+                new Foreach()
+                {
+                    ListProperty = "turn.activity.membersAdded",
+                    ValueProperty = "turn.memberAdded",
+                    Steps = new List<IDialog>()
+                    {
+                        // Note: Some channels send two conversation update events - one for the Bot added to the conversation and another for user.
+                        // Filter cases where the bot itself is the recipient of the message. 
+                        new IfCondition()
+                        {
+                            Condition = "turn.memberAdded.name != turn.activity.recipient.name",
+                            Steps = new List<IDialog>()
+                            {
+                                new SendActivity("Hello, I'm the cards bot. Please send a message to get started!")
+                            }
+                        }
+                    }
+                }
+            };
+
+        }
+    }
+}
