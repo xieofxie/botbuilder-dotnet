624c65bd8e5be6a49b0ebbba731760c208819c4e
Vishwac Sena Kannan <vkannan@microsoft.com>
2019-09-09 11:20:00 -0700

    Adding declarative bot

---
+++ samples/TestBed-Declarative/TestBedBot.cs
@@ -0,0 +1,66 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+//
+// Generated with Bot Builder V4 SDK Template for Visual Studio EchoBot v4.3.0
+
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.Dialogs.Adaptive;
+using Microsoft.Bot.Builder.Dialogs.Debugging;
+using Microsoft.Bot.Builder.Dialogs.Declarative;
+using Microsoft.Bot.Builder.Dialogs.Declarative.Resources;
+using Microsoft.Bot.Schema;
+
+namespace Microsoft.BotBuilderSamples
+{
+    public class TestBedBot : ActivityHandler
+    {
+        private IStatePropertyAccessor<DialogState> dialogStateAccessor;
+        private AdaptiveDialog rootDialog;
+        private readonly ResourceExplorer resourceExplorer;
+        private DialogManager _dialogManager;
+
+        public TestBedBot(ConversationState conversationState, ResourceExplorer resourceExplorer)
+        {
+            this.dialogStateAccessor = conversationState.CreateProperty<DialogState>("RootDialogState");
+            this.resourceExplorer = resourceExplorer;
+
+            // auto reload dialogs when file changes
+            this.resourceExplorer.Changed += (resources) =>
+            {
+                if (resources.Any(resource => resource.Id == ".dialog"))
+                {
+                    Task.Run(() => this.LoadRootDialogAsync());
+                }
+            };
+
+            LoadRootDialogAsync();
+        }
+
+        protected async override Task OnMessageActivityAsync(ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken)
+        {
+            await _dialogManager.OnTurnAsync(turnContext, null, cancellationToken).ConfigureAwait(false);
+        }
+
+        protected override Task OnMembersAddedAsync(IList<ChannelAccount> membersAdded, ITurnContext<IConversationUpdateActivity> turnContext, CancellationToken cancellationToken)
+        {
+            return base.OnMembersAddedAsync(membersAdded, turnContext, cancellationToken);
+        }
+
+        private void LoadRootDialogAsync()
+        {
+            System.Diagnostics.Trace.TraceInformation("Loading resources...");
+
+            var resource = this.resourceExplorer.GetResource("root.dialog");
+            _dialogManager = new DialogManager(DeclarativeTypeLoader.Load<AdaptiveDialog>(resource, resourceExplorer, DebugSupport.SourceRegistry));
+
+            System.Diagnostics.Trace.TraceInformation("Done loading resources.");
+        }
+
+    }
+}
