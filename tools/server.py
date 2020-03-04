5936c2c1c213b8f5e8af38f312c1d13c682f6002
Hualiang Xie <hualxie@microsoft.com>
2020-03-04 11:07:18 +0800

    add train_new_entity_type.py and update bot

--- tools/server.py
+++ tools/server.py
@@ -5,15 +5,20 @@ import os
 from common import common
 
 app = Flask(__name__)
-nlp = None
+nlp_category = None
+nlp_entity = None
 
 @app.route('/recognize/<query>')
 def recognize(query):
-    doc = nlp(query)
+    doc_category = nlp_category(query)
+    doc_entity = nlp_entity(query)
+
     return {
-        "cats": doc.cats
+        "cats": doc_category.cats,
+        "ents": [(ent.label_, ent.text) for ent in doc_entity.ents],
     }
 
 if __name__ == '__main__':
-    nlp = spacy.load(common.OutputCategoryDir)
+    nlp_category = spacy.load(common.OutputCategoryDir)
+    nlp_entity = spacy.load(common.OutputEntityDir)
     app.run()
